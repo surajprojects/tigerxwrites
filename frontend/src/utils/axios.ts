ba36da8d802c6d786daca5efdd37f44b0e41de87
TIGER <suraj23082002@gmail.com>
2026-01-14 21:17:10 +0530

    chore: add frontend env file

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://tigerwrites.suraj23082002.workers.dev/api/v1/",
-    // baseURL: "http://localhost:8787/api/v1/",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
