843d075bbf3ba3aa50fdb80ac699ec755bd0f2f8
TIGER <suraj23082002@gmail.com>
2026-02-07 09:24:36 +0530

    refactor: remove unused Prisma DB connection instance on auxiliary backend

--- auxiliary_backend/src/utils/db.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { PrismaClient } from "../../../backend/prisma/generated/prisma/client.js";
-
-// Prisma Instance
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
