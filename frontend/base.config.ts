56fd057f37a399d99756730723d0c075d54ef0e8
TIGER <suraj23082002@gmail.com>
2026-02-07 09:39:57 +0530

    chore: added auxiliary backend url on frontend

--- frontend/base.config.ts
+++ frontend/base.config.ts
@@ -1,4 +1,5 @@
 // export const VITE_API_URL = "http://localhost:8787/api/v1/";
-export const VITE_SSE_API_URL = "http://localhost:3000/api/v1/";
+// export const VITE_SSE_API_URL = "http://localhost:3000/api/v1/";
+export const VITE_SSE_API_URL = "https://tigerxwrites.onrender.com/api/v1";
 export const VITE_API_URL = "https://api.tigerxwrites.codax.cloud/api/v1/";
 export const VITE_SITE_KEY = "0x4AAAAAACMcX-pMpA2RRH63";
